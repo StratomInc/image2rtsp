c44ed8b599c8f9feb56dcc2d76d3bbe434e2dd37
Nico <nazucca7@gmail.com>
2025-07-09 10:48:27 -0600

    Added launch argument

--- launch/image2rtsp.launch.py
+++ launch/image2rtsp.launch.py
@@ -1,20 +1,24 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import DeclareLaunchArgument, Node
+
 
 def generate_launch_description():
-   config = os.path.join(
-      get_package_share_directory('image2rtsp'),
-      'config',
-      'parameters.yaml'
-      )
+    config = DeclareLaunchArgument(
+        "image2rtsp_config",
+        default_value=os.path.join(
+            get_package_share_directory("image2rtsp"), "config", "parameters.yaml"
+        ),
+    )
 
-   return LaunchDescription([
-      Node(
-         package='image2rtsp',
-         executable='image2rtsp',
-         name='image2rtsp',
-         parameters=[config]
-      )
-   ])
+    return LaunchDescription(
+        [
+            Node(
+                package="image2rtsp",
+                executable="image2rtsp",
+                name="image2rtsp",
+                parameters=[config],
+            )
+        ]
+    )
