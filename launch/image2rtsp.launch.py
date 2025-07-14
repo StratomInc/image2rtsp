12a73a33898a3d72b7a0322042531d8fdfddfc47
apl <nico.zucca@stratom.com>
2025-07-14 10:11:38 -0600

    Fixed issues with launchfile

--- launch/image2rtsp.launch.py
+++ launch/image2rtsp.launch.py
@@ -2,24 +2,26 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    config = DeclareLaunchArgument(
-        "image2rtsp_config",
-        default_value=os.path.join(
-            get_package_share_directory("image2rtsp"), "config", "parameters.yaml"
-        ),
+    default_config_path = os.path.join(
+        get_package_share_directory("image2rtsp"), "config", "parameters.yaml"
     )
 
-    return LaunchDescription(
-        [
-            Node(
-                package="image2rtsp",
-                executable="image2rtsp",
-                name="image2rtsp",
-                parameters=[config],
-            )
-        ]
-    )
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            "image2rtsp_config",
+            default_value=default_config_path,
+            description="Path to the image2rtsp config file"
+        ),
+
+        Node(
+            package="image2rtsp",
+            executable="image2rtsp",
+            name="image2rtsp",
+            parameters=[LaunchConfiguration("image2rtsp_config")]
+        )
+    ])
